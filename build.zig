8735cf3b5b88943b8f074f9e9f8ccd11c11d0f87
schroffl <schroffl@users.noreply.github.com>
2021-01-09 15:33:44 +0100

    Add a single build step that bundles all given resources into a single js file

--- build.zig
+++ build.zig
@@ -8,104 +8,98 @@ pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
 
     const install_dir = std.build.InstallDir{ .Custom = "html" };
-    const js_dir = std.build.InstallDir{ .Custom = "html/js" };
-
-    const wrap_step = WrapJavascriptStep.init(b, "zig-wasm-dsp", .{ .ZigToWebAssembly = "src/main.zig" }, install_dir);
-    const worklet_step = WrapJavascriptStep.init(b, "worklet-wrapper", .{ .JavaScript = "browser/js/worklet.js" }, install_dir);
-    const texture_step = WrapJavascriptStep.init(b, "graph-texture", .{ .JavaScript = "browser/img/lissajous_graph.png" }, js_dir);
     const copy_step = b.addInstallDirectory(.{
         .source_dir = "browser",
         .install_dir = install_dir,
         .install_subdir = "",
     });
 
-    wrap_step.step.dependOn(&copy_step.step);
-    worklet_step.step.dependOn(&wrap_step.step);
-    texture_step.step.dependOn(&worklet_step.step);
+    const resource_step = ResourceStep.init(b, "resources", install_dir);
+    resource_step.addResource("worklet-wrapper", "browser/js/worklet.js");
+    resource_step.addResource("graph-texture", "browser/img/lissajous_graph.png");
 
-    b.default_step.dependOn(&texture_step.step);
+    const wasm_step = resource_step.addWasmResource("zig-wasm-dsp", "src/main.zig");
+    wasm_step.setBuildMode(mode);
+
+    resource_step.step.dependOn(&copy_step.step);
+    b.default_step.dependOn(&resource_step.step);
 }
 
-const WrapJavascriptStep = struct {
+const ResourceStep = struct {
     builder: *Builder,
     step: std.build.Step,
     install_dir: std.build.InstallDir,
-    name: []const u8,
+    filename: []const u8,
+    resources: std.StringHashMap([]const u8),
+    wasm_resources: std.StringHashMap(*std.build.LibExeObjStep),
 
-    source_type: SourceType,
-    wasm_lib_step: ?*std.build.LibExeObjStep,
+    pub fn init(b: *Builder, filename: []const u8, dir: std.build.InstallDir) *ResourceStep {
+        var self = b.allocator.create(ResourceStep) catch unreachable;
 
-    const SourceType = union(enum) {
-        JavaScript: []const u8,
-        ZigToWebAssembly: []const u8,
-    };
-
-    pub fn init(b: *Builder, name: []const u8, root_src: SourceType, dir: std.build.InstallDir) *WrapJavascriptStep {
-        var self = b.allocator.create(WrapJavascriptStep) catch unreachable;
-
-        self.step = std.build.Step.init(.Custom, "Wrap Javascript", b.allocator, make);
         self.builder = b;
-        self.name = name;
+        self.step = std.build.Step.init(.Custom, "wrap-resources", b.allocator, make);
         self.install_dir = dir;
-        self.source_type = root_src;
-
-        self.wasm_lib_step = switch (root_src) {
-            .ZigToWebAssembly => |wasm_path| result: {
-                var lib_step = b.addStaticLibrary(name, wasm_path);
-                lib_step.setTarget(.{
-                    .cpu_arch = .wasm32,
-                    .os_tag = .freestanding,
-                });
-
-                self.step.dependOn(&lib_step.step);
-
-                break :result lib_step;
-            },
-            else => null,
-        };
+        self.filename = filename;
+        self.resources = std.StringHashMap([]const u8).init(b.allocator);
+        self.wasm_resources = std.StringHashMap(*std.build.LibExeObjStep).init(b.allocator);
 
         return self;
     }
 
-    pub fn setBuildMode(self: *WrapJavascriptStep, mode: std.builtin.Mode) void {
-        if (self.wasm_lib_step) |lib_step|
-            lib_step.setBuildMode(mode);
+    pub fn addResource(self: *ResourceStep, name: []const u8, file_path: []const u8) void {
+        self.resources.putNoClobber(name, file_path) catch unreachable;
+    }
+
+    pub fn addWasmResource(self: *ResourceStep, name: []const u8, root_src: []const u8) *std.build.LibExeObjStep {
+        var lib_step = self.builder.addStaticLibrary(name, root_src);
+        lib_step.setTarget(.{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
+        });
+
+        self.step.dependOn(&lib_step.step);
+        self.wasm_resources.putNoClobber(name, lib_step) catch unreachable;
+        return lib_step;
     }
 
     fn make(step: *std.build.Step) !void {
-        const self = @fieldParentPtr(WrapJavascriptStep, "step", step);
-        const lib_output_path = switch (self.source_type) {
-            .JavaScript => |js_path| js_path,
-            .ZigToWebAssembly => self.wasm_lib_step.?.getOutputPath(),
-        };
+        var self = @fieldParentPtr(ResourceStep, "step", step);
+        var wasm_it = self.wasm_resources.iterator();
 
-        const install_filename = self.builder.fmt("{s}.js", .{self.name});
-        const install_dirname = self.builder.getInstallPath(self.install_dir, "");
+        while (wasm_it.next()) |wasm_entry| {
+            const lib_output_path = wasm_entry.value.getOutputPath();
+            self.addResource(wasm_entry.key, lib_output_path);
+        }
+
+        var resource_it = self.resources.iterator();
+
+        const install_filename = self.builder.fmt("{s}.js", .{self.filename});
         const install_path = self.builder.getInstallPath(self.install_dir, install_filename);
-
-        try self.builder.makePath(install_dirname);
-
-        var file = try std.fs.cwd().openFile(lib_output_path, .{ .read = true });
-        defer file.close();
-
-        const stat = try file.stat();
-        const content = try file.readToEndAlloc(self.builder.allocator, stat.size);
-        defer self.builder.allocator.free(content);
-
-        const base64_len = std.base64.Base64Encoder.calcSize(content.len);
-        const base64_buffer = try self.builder.allocator.alloc(u8, base64_len);
-        defer self.builder.allocator.free(base64_buffer);
-
         var out_file = try std.fs.cwd().createFile(install_path, .{});
         defer out_file.close();
-        var writer = out_file.writer();
 
-        try writer.writeAll("window[\"");
-        try writer.writeAll(self.name);
-        try writer.writeAll("\"] = \"");
+        var build_dir = try std.fs.cwd().openDir(self.builder.build_root, .{});
+        defer build_dir.close();
 
-        const final_base64 = std.base64.standard_encoder.encode(base64_buffer, content);
-        try writer.writeAll(final_base64);
-        try writer.writeAll("\";\n");
+        while (resource_it.next()) |entry| {
+            var in_file = try build_dir.openFile(entry.value, .{ .read = true });
+            defer in_file.close();
+
+            const stat = try in_file.stat();
+            const content = try in_file.readToEndAlloc(self.builder.allocator, stat.size);
+            defer self.builder.allocator.free(content);
+
+            const base64_len = std.base64.Base64Encoder.calcSize(content.len);
+            const base64_buffer = try self.builder.allocator.alloc(u8, base64_len);
+            defer self.builder.allocator.free(base64_buffer);
+
+            const final_base64 = std.base64.standard_encoder.encode(base64_buffer, content);
+
+            try out_file.writeAll("window[\"");
+            try out_file.writeAll(entry.key);
+            try out_file.writeAll("\"] = \"");
+            try out_file.writeAll(final_base64);
+            try out_file.writeAll("\";\n");
+        }
     }
 };
